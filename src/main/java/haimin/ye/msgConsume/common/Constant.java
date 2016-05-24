8aa91269039e8fbbf1821a4ad8824804f4236d44
MichaelYe <haiye@paypal.com>
2016-05-24 17:38:48 +0800

    format

--- src/main/java/haimin/ye/msgConsume/common/Constant.java
+++ src/main/java/haimin/ye/msgConsume/common/Constant.java
@@ -1,13 +1,13 @@
 package haimin.ye.msgConsume.common;
 
 public class Constant {
-    
-  public static final Message END_TAG = new Message("END_TAG");
-  
-  public static final int DEFAULT_NUM_THREADS_PUBLISH = 10;
-  
-  public static final int DEFAULT_NUM_THREADS_CONSUME = 10;
-  
-  public static final int DEFAULT_QUEUE_SIZE = 100;
+
+    public static final Message END_TAG = new Message("END_TAG");
+
+    public static final int DEFAULT_NUM_THREADS_PUBLISH = 10;
+
+    public static final int DEFAULT_NUM_THREADS_CONSUME = 10;
+
+    public static final int DEFAULT_QUEUE_SIZE = 100;
 
 }
